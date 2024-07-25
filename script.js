e9f12ea1f225d6ed425fb40dd1a4ab2e4fa67e1b
Armi-prog <armi-a@yandex.ru>
2024-07-25 10:25:04 +0400

    create file mthod

---
+++ script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const person = {
+    firstName: "Anna",
+    lastName: "Karenina",
+    age: 32,
+    sayHello(country) {
+         console.log(`Hello I am ${person.firstName} ${person.lastName}, I am ${person.age}, from ${country}.`)
+    }
+};
+
+person.sayHello("Russia");
