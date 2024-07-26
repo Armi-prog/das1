23fc88976ef59d7716ae657d402a87fac25c6ea1
Armi-prog <armi-a@yandex.ru>
2024-07-26 12:34:54 +0400

    add filmsApp

--- script.js
+++ script.js
@@ -9,4 +9,21 @@ const person = {
     }
 };
 
+
+const countOfFilms = +prompt("Քանի՞ ֆիլմ եք վերջերս դիտել");
+
+const DB = {g
+    count: countOfFilms,
+    movies: {},
+    actors: {},
+    geners: [],
+    private: false
+};
+
+const a = prompt("Ո՞ր ֆիլմն եք վերջերս դիտել");
+const b = prompt("Ինչքան՞ եք գնահատում այդ ֆիլմը");
+
+DB.movies[a] = b;
+console.log(DB);
+
  
