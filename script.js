d2bcafcc515cf737a50b10cc2915703fb23cf989
Armi-prog <armi-a@yandex.ru>
2024-07-27 21:44:28 +0400

    added status

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 "use strict";
 
 const countOfFilms = +prompt("Քանի՞ ֆիլմ եք վերջերս դիտել");
@@ -7,28 +8,33 @@ const DB = {
     movies: {},
     actors: {},
     geners: [],
+    status: null,
     private: false
 };
 
-
-for (let i = 0; 1 < 2; i++) {
+for (let i = 0; i < 2; i++) {
     const filmName = prompt("Ո՞ր ֆիլմն եք վերջերս դիտել");
     const filmRate = +prompt(`Ինչքան՞ եք գնահատում ${filmName} ֆիլմը`);
 
-    if(filmName != null && filmRate != null && filmName != "" && filmRate != "" && filmName.length < 50 ) {
+    if(filmName != null && filmRate != null && filmName != "" && filmRate != "" ) {
         DB.movies[filmName] = filmRate;
         console.log("Resolve");
+    }
         else {
             console.log("Reject");
             i--;
         }
-}
-
 
 }
 
-
+if (DB.count < 10) {
+    DB.status = "Դուք բավականին քիչ ֆիլմ եք դիտել";
+} else if (DB.count >= 10 && DB.count < 30) {
+    DB.status = "Դուք դասական ֆիլմ դիտող եք";
+} else if (DB.count >= 30) {
+    DB.status = "Դուք կինոման եք";
+} else {
+    console.log("Տեղի է ունեցել սխալ");
+}
 
 console.log(DB);
-
- 
