a58ebac0a5a04f380c5ec8fac17d4c64d0e33b58
Armi-prog <armi-a@yandex.ru>
2024-07-27 23:27:31 +0400

    added functions

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 
 "use strict";
 
-const countOfFilms = +prompt("Քանի՞ ֆիլմ եք վերջերս դիտել");
+let countOfFilms;
+
+
+
+  init();
 
 const DB = {
     count: countOfFilms,
@@ -12,29 +16,47 @@ const DB = {
     private: false
 };
 
-for (let i = 0; i < 2; i++) {
-    const filmName = prompt("Ո՞ր ֆիլմն եք վերջերս դիտել");
-    const filmRate = +prompt(`Ինչքան՞ եք գնահատում ${filmName} ֆիլմը`);
+rememberAndCreateUserFilms();
+detectUserPersonalLevel();
+checkDBPrivateStatus(DB.private);
 
-    if(filmName != null && filmRate != null && filmName != "" && filmRate != "" ) {
-        DB.movies[filmName] = filmRate;
-        console.log("Resolve");
+function init() {
+    countOfFilms = +prompt("Քանի՞ ֆիլմ եք վերջերս դիտել");
+    while (countOfFilms == "" || countOfFilms == null || isNaN(countOfFilms)) {
+        countOfFilms = +prompt("Քանի՞ ֆիլմ եք վերջերս դիտել");   
     }
-        else {
-            console.log("Reject");
-            i--;
-        }
-
 }
 
-if (DB.count < 10) {
-    DB.status = "Դուք բավականին քիչ ֆիլմ եք դիտել";
-} else if (DB.count >= 10 && DB.count < 30) {
-    DB.status = "Դուք դասական ֆիլմ դիտող եք";
-} else if (DB.count >= 30) {
-    DB.status = "Դուք կինոման եք";
-} else {
-    console.log("Տեղի է ունեցել սխալ");
+function rememberAndCreateUserFilms() {
+    for (let i = 0; i < 2; i++) {
+        const filmName = prompt("Ո՞ր ֆիլմն եք վերջերս դիտել");
+        const filmRate = +prompt(`Ինչքան՞ եք գնահատում ${filmName} ֆիլմը`);
+    
+        if(filmName != null && filmRate != null && filmName != "" && filmRate != "" ) {
+            DB.movies[filmName] = filmRate;
+            console.log("Resolve");
+        }
+            else {
+                console.log("Reject");
+                i--;
+            }
+    }
+}
+
+function detectUserPersonalLevel() {
+    if (DB.count < 10) {
+        DB.status = "Դուք բավականին քիչ ֆիլմ եք դիտել";
+    } else if (DB.count >= 10 && DB.count < 30) {
+        DB.status = "Դուք դասական ֆիլմ դիտող եք";
+    } else if (DB.count >= 30) {
+        DB.status = "Դուք կինոման եք";
+    } else {
+        console.log("Տեղի է ունեցել սխալ");
+    }
+}
+
+function checkDBPrivateStatus(state) {
+    state ? console.log("Sorry but DB is not visible") : console.log(DB);
 }
 
 console.log(DB);
